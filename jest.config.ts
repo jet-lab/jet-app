cc5dd88db22789230cc1a22937278c1969a052e8
Davide <scalzodavide@yahoo.it>
2022-05-13 12:32:48 +0100

    added sample tests and code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,15 @@
 export default {
   clearMocks: true,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      // lines: 1,
+      branches: 2,
+      // functions: 0,
+      // statements: 0
+    }
+  },
+  collectCoverageFrom: [    "**/*.{js,jsx,ts,tsx}"],
   coverageDirectory: "coverage",
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 }
