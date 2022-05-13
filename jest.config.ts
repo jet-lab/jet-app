c120dd2eee085d8f5556350d45390f788e5db12a
Davide Scalzo <scalzodavide@yahoo.it>
2022-05-13 18:55:41 +0100

    tweak jest.config.js

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,17 @@
-export default {
+const config =  {
   clearMocks: true,
   collectCoverage: false,
   coverageThreshold: {
     global: {
-      // lines: 1,
-      branches: 2
-      // functions: 0,
-      // statements: 0
+      lines: 1.2,
+      branches: 2.1,
+      functions: 1.7,
+      statements: 1.1
     }
   },
   collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts', '<rootDir>/src/__mocks__/localStorage.ts']
 };
+
+export default config
