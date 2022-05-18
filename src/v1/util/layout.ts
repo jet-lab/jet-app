6e087f4d1f3860285f9f39452bad045adfb91782
Davide Scalzo <scalzodavide@yahoo.it>
2022-05-18 17:43:34 +0100

    fixed dependencies [maybe]

--- src/v1/util/layout.ts
+++ src/v1/util/layout.ts
@@ -1,8 +1,10 @@
 import { BN } from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
 import * as BL from '@solana/buffer-layout';
+import { JetMarketReserveInfo, ObligationPositionStruct } from '@jet-lab/jet-engine';
+import { ReserveStateStruct } from '../models/JetTypes';
 
-export class NumberField extends BL.Layout {
+export class NumberField extends BL.Layout<BN> {
   decode(b: Uint8Array, offset?: number): BN {
     const start = offset === undefined ? 0 : offset;
     const data = b.slice(start, start + this.span);
@@ -17,7 +19,7 @@ export class NumberField extends BL.Layout {
   }
 }
 
-export class SignedNumberField extends BL.Layout {
+export class SignedNumberField extends BL.Layout<BN> {
   decode(b: Uint8Array, offset?: number): BN {
     const start = offset === undefined ? 0 : offset;
     const data = b.slice(start, start + this.span);
@@ -32,7 +34,7 @@ export class SignedNumberField extends BL.Layout {
   }
 }
 
-export class PubkeyField extends BL.Layout {
+export class PubkeyField extends BL.Layout<PublicKey> {
   constructor(property?: string) {
     super(32, property);
   }
@@ -70,15 +72,15 @@ export function pubkeyField(property?: string): PubkeyField {
 
 const MAX_RESERVES = 32;
 
-const ReserveInfoStruct = BL.struct([
+const ReserveInfoStruct = BL.struct<JetMarketReserveInfo>([
   pubkeyField('reserve'),
-  BL.blob(80, '_UNUSED_0_'),
+  BL.blob(80, '_UNUSED_0_') as any,
   numberField('price'),
   numberField('depositNoteExchangeRate'),
   numberField('loanNoteExchangeRate'),
   numberField('minCollateralRatio'),
   BL.u16('liquidationBonus'),
-  BL.blob(158, '_UNUSED_1_'),
+  BL.blob(158, '_UNUSED_1_') as any,
   u64Field('lastUpdated'),
   BL.u8('invalidated'),
   BL.blob(7, '_UNUSED_1_')
@@ -87,26 +89,26 @@ const ReserveInfoStruct = BL.struct([
 export const MarketReserveInfoList = BL.seq(ReserveInfoStruct, MAX_RESERVES);
 
 /// Reserve
-export const ReserveStateLayout = BL.struct([
+export const ReserveStateLayout = BL.struct<ReserveStateStruct>([
   i64Field('accruedUntil'),
   numberField('outstandingDebt'),
   numberField('uncollectedFees'),
   u64Field('totalDeposits'),
   u64Field('totalDepositNotes'),
   u64Field('totalLoanNotes'),
-  BL.blob(416, '_UNUSED_0_'),
+  BL.blob(416, '_UNUSED_0_') as any,
   u64Field('lastUpdated'),
   BL.u8('invalidated'),
   BL.blob(7, '_UNUSED_1_')
 ]);
 
 /// Obligation
-export const PositionInfo = BL.struct([
+export const PositionInfo = BL.struct<ObligationPositionStruct>([
   pubkeyField('account'),
   numberField('amount'),
   BL.u32('side'),
   BL.u16('reserveIndex'),
-  BL.blob(66, '_reserved')
+  BL.blob(66, '_reserved') as any
 ]);
 
 export const PositionInfoList = BL.seq(PositionInfo, 16, 'positions');
