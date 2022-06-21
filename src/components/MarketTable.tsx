d8e53df31d43a14c2cd6cbef4d3d4986905af22e
Davide Scalzo <scalzodavide@yahoo.it>
2022-06-21 10:20:31 +0100

    integrated radar endpoint

--- src/components/MarketTable.tsx
+++ src/components/MarketTable.tsx
@@ -172,7 +172,7 @@ export function MarketTable(): JSX.Element {
                       <td
                         onClick={() => {
                           setPoolDetail(pool);
-                          setCurrentPool(pool)
+                          setCurrentPool(pool);
                         }}
                         className="reserve-detail text-btn bold-text">
                         {pool.symbol} {dictionary.cockpit.detail}
